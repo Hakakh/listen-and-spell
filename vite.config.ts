63a08f0d4d221793c185ae8d104a2f21c2bfc721
Hakakh <baitapvenhat@gmail.com>
2026-09-20 16:24:43 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig, loadEnv } from 'vite';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-        base: '/',
+         base: '/listen-and-spell/',
         server: {
           port: 3000,
           host: '0.0.0.0',
